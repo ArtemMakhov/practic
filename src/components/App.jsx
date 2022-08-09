dc7698a0986c4b80d90f88f595fc43bb6f06e199
Artem Makhov <makhovartem23@gmail.com>
2022-08-09 17:02:19 +0300

    del old files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,6 @@
 import { Box } from './Box';
 import { GlobalStyle } from './GlobalStyle';
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+
 
 
 
@@ -15,15 +8,7 @@ export const App = () => {
   return (
 <>
   <Box>
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats} />
-    <Statistics title="Upload stats" stats={data} />
-    <FriendList friends={friends} />
-    <TransactionHistory items={transactions} />
+
   </Box>
   <GlobalStyle/>
 </>
